ccf33c3c268ddb621108e28df231942a77306a51
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-03-27 13:03:58 +0530

    main-page completed

--- web/src/app/home/main-page/main-page.component.ts
+++ web/src/app/home/main-page/main-page.component.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-main-page',
     templateUrl: './main-page.component.html',
-    styleUrls: ['./main-page.component.css']
+    styleUrls: ['./main-page.component.scss']
 })
 
-export class MainPageComponent {}
+export class MainPageComponent {
+    fileName: string = '';
+    isFileUploaded: boolean = false;
+    isButtonClicked: boolean = false;
+
+    constructor(private http: HttpClient) { }
+
+    onFileSelected(event: any): void {
+        const file: File = event.target.files[0];
+        if (file) {
+            this.fileName = file.name;
+            const formData = new FormData();
+            formData.append("thumbnail", file);
+            const upload$ = this.http.post("/api/thumbnail-upload", formData);
+            upload$.subscribe();
+            this.isFileUploaded = true; 
+        }
+    }
+
+    showResults(): void {
+        this.isButtonClicked = true;
+        window.setTimeout(() => {
+            this.isButtonClicked = false;
+            this.isFileUploaded = false;
+        }, 3000);
+    }
+}
